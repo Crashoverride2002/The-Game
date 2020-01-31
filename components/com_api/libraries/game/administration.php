97fcd58d446a67bb82f54fe89cad0eba1352975e
Kent Hansen <HansenKentc@gmail.com>
2020-01-31 05:24:38 +0100

    New Folder and updates.

--- components/com_api/libraries/game/administration.php
+++ components/com_api/libraries/game/administration.php
@@ -2,6 +2,7 @@
 defined('_JEXEC') or die;
 jimport('joomla.application.component.model');
 jimport('joomla.user.authentication');
+jimport('joomla.filesystem.file');
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
@@ -12,22 +13,34 @@ class APIGameAdministration extends Game
 {
 
     abstract public function administrate();
+    public function get_game_list()
+    {
 
-    public function edit() {
+    }
+    public function upload_game()
+
         $authenticate = JAuthentication::getInstance();
         $app = JFactory::getApplication();
         $user = JFactory::getUser();
         if (!Factory::getUser()->authorise('core.admin'))
         {
-            throw new NotAllowed(Text::_('JERROR_ALERTNOAUTHOR'), 403);
+            throw new NotAllowed(Text::_('JERROR_ALERTNOAUTHOR'),403);
         }
         else
         {
             $db = $this->getDbo();
-            $item_id = $this->input->get('item_id',0,'int');
-            $title = $this->input->get('title','','string');
-            $coordinate = $this->input->get('coordinate','','string');
-            
+            $fk_user_id = $this->input->get('user_id',0,'int');
+            $game_name = $this->input->get('title','','string');
+            $video_name = $this->input->get('video_name','','string');
+            $video_data = $this->input->get('video_data','','string');
+            if (!empty($video_data))
+            {
+                $filename = JFile::makeSafe($video_name);
+                $dest = DS."uploads".DS.$filename;
+                JFile::write($filename,$video_data);
+                return 
+            }
         }
     }
+    
 }
