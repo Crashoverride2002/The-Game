02ab0998f7c63e9880f5478fa099d1e165792b8c
Kent Hansen <HansenKentc@gmail.com>
2020-02-02 08:22:25 +0100

    New SQL FILE for game

---
+++ tmp/GameDef.sql
@@ -0,0 +1,28 @@
+use TheGame;
+
+CREATE TABLE `Game_gamedef` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `fk_game_type_id` int(11) NOT NULL,
+  `video_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `fk_auth_user_id` int(11) NOT NULL,
+  `prolog` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `GAME_geoloc` (
+    `id` int(11) not null auto_increment,
+    `game_id` int(11) not null,
+    `street_name` varchar(350) not null default 'No where.',
+    `geo_loc` varchar(60) not null default '0,000000;0,000000',
+    `fk_user_id` int(11) not null,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `GAME_gametypes` (
+    `id` int(11) not null auto_increment,
+    `type_name` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+    `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+    `created_by_user_id` int(11) not null,
+    primary key (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
